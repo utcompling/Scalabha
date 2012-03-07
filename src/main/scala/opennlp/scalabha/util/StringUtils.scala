872e4ab3a4a79a8dc3f4f5349819b8931e10e8c6
Jason Baldridge <jasonbaldridge@gmail.com>
2012-03-06 19:24:46 -0600

    Scalabha ready for ATA HW4.

--- src/main/scala/opennlp/scalabha/util/StringUtils.scala
+++ src/main/scala/opennlp/scalabha/util/StringUtils.scala
@@ -2,11 +2,17 @@ package opennlp.scalabha.util
 
 import org.apache.commons.lang3.StringEscapeUtils.unescapeHtml4
 
+/**
+ * A very simple tokenizer that pulls most puncuation off the characters.
+ * Given a raw string, tokenize it with a simple regular expression, returning 
+ * an IndexedSeq[String] with one token per element.
+ */
 object SimpleTokenizer {
-  def apply(text: String) = text
-    .replaceAll("""([\?!()\";\|\[\].,'])""", " $1")
+  def apply(text: String): IndexedSeq[String] = text
+    .replaceAll("""([\?!()\";\|\[\].,'])""", " $1 ")
     .trim
     .split("\\s+")
+    .toIndexedSeq
 }
 
 object StringUtils {
