838b4c6b8b964f22377aec81e6c0fc6caf24e6bf
Dan Garrette <dhgarrette@gmail.com>
2012-03-11 18:30:28 -0500

    delete RandomFreqCounter.scala

--- src/main/scala/opennlp/scalabha/tag/support/RandomFreqCounter.scala
+++
@@ -1,8 +0,0 @@
-package opennlp.scalabha.tag.support
-
-import opennlp.scalabha.util.CollectionUtils._
-import opennlp.scalabha.util.Probability
-import opennlp.scalabha.util.Probability._
-import collection.mutable.{ Map => MMap }
-import util.Random
-
