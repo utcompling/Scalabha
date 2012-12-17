a76cbbf803b1c38b31370f2a9e662434d45237e7
Dan Garrette <dhgarrette@gmail.com>
2012-12-17 14:29:20 -0600

    cleaned up CCG CKY parser to be easier to read and more scala idiomatic

--- src/main/scala/opennlp/scalabha/ccg/Cky.scala
+++ src/main/scala/opennlp/scalabha/ccg/Cky.scala
@@ -1,62 +1,48 @@
 package opennlp.scalabha.ccg
 
+import scala.collection.breakOut
+
 /** A CkyParser that uses the given lexicon and set of rules. */
-class CkyParser (lexicon: Map[String,Set[Cat]], rules: List[Rule]) {
+class CkyParser(lexicon: Map[String, Set[Cat]], rules: List[Rule]) {
 
-  import collection.mutable.HashSet
+  def apply(sentence: String): Set[Cat] = apply(sentence.split(" ").toIndexedSeq)
 
-  def apply (sentence: String): Set[Cat] = apply(sentence.split(" ").toIndexedSeq)
-
-  def apply (tokens: IndexedSeq[String]): Set[Cat] = {
+  def apply(tokens: IndexedSeq[String]): Set[Cat] = {
     val numItems = tokens.length
 
-    tokens.foreach {
-      token => {
-	lexicon.get(token) match {
-	  case None => 
-	    throw new MissingLexicalEntryException("\n\nError: word '" + token + "' not in lexicon!\n")
-	  case _ =>
-	} 
-      }
+    tokens.foreach { token =>
+      if (!lexicon.contains(token))
+        throw new MissingLexicalEntryException("\n\nError: word '" + token + "' not in lexicon!\n")
     }
 
-    val chart: Array[Array[Set[Cat]]] = 
-      Array.fill(numItems, numItems)(new collection.immutable.HashSet[Cat]())
-    
+    val chart: Array[Array[Set[Cat]]] = Array.fill(numItems, numItems)(Set[Cat]())
+
     for (j <- (0 until numItems)) {
       chart(j)(j) = lexicon(tokens(j))
 
-      for (i <- (0 to j-1).reverse) {
-	val fill = new HashSet[Cat]
-
-	for (k <- i to j-1) {
-
-	  for (left <- chart(i)(k)) {
-
-	    for (right <- chart(k+1)(j)) {
-
-	      rules.foreach { rule =>
-                val (leftUniq, rightUniq) = Cat.makeAllVarsUniq(left,right)
-		rule(leftUniq, rightUniq) match {
-		  case Some(result) => fill.add(result)
-		  case None => 
-		}
-	      }
-	    }
-	  }
-	  chart(i)(j) = fill.toSet
-	}
+      for (i <- (0 to j - 1).reverse) {
+        chart(i)(j) =
+          (for (
+            k <- i to j - 1;
+            left <- chart(i)(k);
+            right <- chart(k + 1)(j);
+            (leftUniq, rightUniq) = Cat.makeAllVarsUniq(left, right);
+            rule <- rules;
+            result <- rule(leftUniq, rightUniq)
+          ) yield result)(breakOut)
       }
     }
-    chart(0)(numItems-1)
+    chart(0)(numItems - 1)
   }
-}    
+}
 
 /** A companion object for the CkyParser class. */
 object CkyParser {
-  
-  // Create a CkyParser object with the provided lexicon and using all
-  // possible rules.
-  def apply (lexicon: Map[String, Set[Cat]]) = new CkyParser(lexicon, Rule.allRules)
+
+  /**
+   * Create a CkyParser object with the provided lexicon and using all
+   * possible rules.
+   */
+  def apply(lexicon: Map[String, Set[Cat]]) = new CkyParser(lexicon, Rule.allRules)
 
 }
